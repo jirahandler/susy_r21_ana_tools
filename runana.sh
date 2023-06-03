5aaa2732fbbd6e15a93196efc08d5688bbfa05bd
SammyG <goswamisoumya01@gmail.com>
2023-06-02 22:59:36 -0500

    22:59:36 commit

--- runana.sh
+++ runana.sh
@@ -1,3 +1,3 @@
-for f in 410470/410470.root; do ./bin/tt_ana $f ; done
-for f0 in lf_all/*.root; do ./bin/sus_ana $f0 ; done
-for f1 in hf_all/*.root; do ./bin/sus_ana $f1 ; done
+for f in /eos/user/s/sgoswami/public/syst_samples/410470/410*.root; do ./bin/tt_ana $f ; done
+for f0 in /eos/user/s/sgoswami/public/syst_samples/lf_all/*/*.root; do ./bin/sus_ana $f0 ; done
+for f1 in /eos/user/s/sgoswami/public/syst_samples/hf_all/*/*.root; do ./bin/sus_ana $f1 ; done
